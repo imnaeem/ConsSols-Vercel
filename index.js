749512865b701c942f3ac7e38220a29dad6398cf
imnaeem <ceinfo.pk@gmail.com>
2022-12-14 17:33:32 +0500

    Final

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import cors from "cors";
-import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-
 import authRoute from "./routes/auth.js";
 import companyRoutes from "./routes/companyRoutes.js";
 import clientRoutes from "./routes/clientRoutes.js";
 import homeRoutes from "./routes/homeRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import dotenv from "dotenv";
+import dbConnection from "./dbConnection.js";
 
 const app = express();
 
@@ -28,26 +26,17 @@ app.use("/api/admin", adminRoutes);
 app.use("/api", homeRoutes);
 app.use("/api/user", authRoute);
 
-// // For Production Build
+// For Production Build
 
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // DB Connection
 
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
-  )
-  .catch((error) => console.log(`${error} did not connect`));
+dbConnection(app);
